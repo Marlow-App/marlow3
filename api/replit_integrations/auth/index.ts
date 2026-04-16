d56105d95e231933f02b4f70516a37eaf97c13e1
Marlow-App <bymarloww@gmail.com>
2026-04-16 14:51:54 +0900

    index

--- api/replit_integrations/auth/index.ts
+++ api/replit_integrations/auth/index.ts
@@ -1,3 +1,3 @@
-export { setupReplitAuth } from "./replitAuth.js";
-export { authStorage, type IAuthStorage } from "./storage.js"; // ADDED .js
-export { registerAuthRoutes } from "./routes.js"; // ADDED .js
+export { setupAuth as setupReplitAuth, isAuthenticated } from "./replitAuth.js";
+export { authStorage, type IAuthStorage } from "./storage.js";
+export { registerAuthRoutes } from "./routes.js";
