3ef132731eaaaad7e7aad95be173c0c863e007f5
Marlow-App <bymarloww@gmail.com>
2026-04-16 16:48:07 +0900

    o

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": path.resolve(process.cwd(), "client/src"),
+      "@shared": path.resolve(process.cwd(), "shared"),
+      "@assets": path.resolve(process.cwd(), "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
+  root: path.resolve(process.cwd(), "client"),
   build: {
-    // Changed this to "dist" to match your vercel.json outputDirectory
-    outDir: path.resolve(__dirname, "../dist"),
+    outDir: path.resolve(process.cwd(), "dist"),
     emptyOutDir: true,
   },
   server: {
